af0c6248c848a5a9e8ad12eab279494bc5dca0ce
brarnedo <brarnedo@gmail.com>
2025-07-15 14:36:34 -0300

    commit BA

--- src/store/slices/getArchivadas/thunks.js
+++ src/store/slices/getArchivadas/thunks.js
@@ -1,14 +1,22 @@
-
-
+import { useSelector } from 'react-redux';
 
 export const getArchivadas = () => {
-
+   
     return async (dispatch, getState) => {
-
+         
+       
         try {
 
+            const { session } = getState().auth;
+            console.log(session)
+
+            const myBody = JSON.stringify( {
+                "session": session
+            })
+        
             const configRequest = {
-                method:"GET"
+                method:"POST",
+                body:myBody
             }
 
             const response = await fetch ("https://portal2-des.iplan.com.ar/comunicaciones/notificaciones_new/api/notifications/get.php?type=programadas", configRequest);
